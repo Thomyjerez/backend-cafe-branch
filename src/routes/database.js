4b8555b04874030c11c6c58ce180eab785ade8be
thomas jerez <tjerez@outlook.com>
2022-11-10 18:50:03 -0300

    correcciones

--- src/routes/database.js
+++ src/routes/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 // localhost: 127.0.0.1
-const url = 'mongodb://localhost:27017/cafe-branch'
+// const url = 'mongodb://localhost:27017/cafe-branch' // BD-local
+ const url = 'mongodb+srv://ThomasJerez:Tho0mas100@cluster0.x5xfs5b.mongodb.net/cafe-branch'
 
 // pedirle a mongose que se conecte a la base de datos
 mongoose.connect(url);
